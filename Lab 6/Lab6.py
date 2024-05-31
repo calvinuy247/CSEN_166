d9c6af2d2a7ebbf2847ba0497515e553bc43e5b1
adian12 <adianalvarado831@gmail.com>
2024-05-31 13:35:18 -0700

    Pushing new lines of code
    I'm going to wait for tips on sections I was confused on. Hopefully he goes over it during the introduction powerpoint.

--- Lab 6/Lab6.py
+++ Lab 6/Lab6.py
@@ -1,22 +1,61 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
 import tensorflow as tf
 from tensorflow import keras
+from keras import datasets, layers, models
+import numpy as np
 
+#Loading the dataset
 fashion_mnist = keras.datasets.fashion_mnist
 (x_train, y_train),(x_test,y_test) = fashion_mnist.load_data()
-x_train,x_test =x_train/255.0,x_test/255.0 #normalize the pixel values to be in [0,1]
 
-#note we need to flatten the image to a vector, to serve as the input layer of the network
-
-#1a: Select one image from each class fo the dataset to display as a grey-scale image
+#Normalize the pixel values to be in [0,1]
+x_train,x_test = x_train/255.0,x_test/255.0
 
 
+#1a: Select one image from each class of the dataset to display as a grey-scale image
+import matplotlib.pyplot as plt
+plt.imshow(x_test[19,:,:], cmap='gray', vmin=0, vmax=1)
+plt.show()
+### Need to finish this section, going to see if there are any tips during lab intro. ###
+
+#Setting the model
+model = models.Sequential()
 
 
+#Adding layers
+#Note we need to flatten the image to a vector, to serve as the input layer of the network
+model.add(layers.Flatten())
 
+#Creating the hidden layer with 512 nodes and ReLU activation function
+model.add(layers.Dense(512, activation='relu'))
+
+#Creating the output layer with 10 nodes and softmax activation function
+model.add(layers.Dense(10, activation='softmax'))
+
+
+#Printing summary and compiling the model with adam optimizer and sparse... loss function  
+model.summary()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+
+#Training the model for 5 epochs and a batch size of 32
+model.fit(x_train, y_train, epochs=5, batch_size=32)
+
+
+#Calculating the test's loss and accuracy
+loss, accuracy = model.evaluate(x_test, y_test)
+print(accuracy)
+
+
+#Calculating the model's predicted probalilty 
+predProbablility = model.predict(x_test)
+
+#y_test_hat -> predicted value
+y_test_hat = np.argmax(predProbablility, axis=1)
 
 #2a: Give the recognition accuracy rate of the test set, and show the confusion matrix
- 
- 
+from sklearn.metrics import confusion_matrix
+
+### Also need to finish this section. ###
+#Need to replace the ... with labels, like in mnist_cnn.py 
+#confuMatrix = confusion_matrix(y_test, y_test_hat, ...)
