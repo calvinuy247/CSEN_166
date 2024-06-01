5350006be370fdb10d2b5b8863d3023a81918762
adian12 <adianalvarado831@gmail.com>
2024-05-31 18:48:44 -0700

    Finished remaining steps
    Going to take a break and double check, then work on report.

--- Lab 6/Lab6.py
+++ Lab 6/Lab6.py
@@ -13,23 +13,41 @@ fashion_mnist = keras.datasets.fashion_mnist
 #Normalize the pixel values to be in [0,1]
 x_train,x_test = x_train/255.0,x_test/255.0
 
-
-#1a: Select one image from each class of the dataset to display as a grey-scale image
+#Selecting an image from each class to display as a grey-scale image
 import matplotlib.pyplot as plt
+classes = ["T-shirt/top", "Trouser", "Pullover", "Dress", "Coat", "Sandal", "Shirt", "Sneaker", "Bag", "Ankle boot"]
 
+#Stores an image for each class
+imagesEachClass = {}
 
+#Traversing every image in the test dataset
+for image in range(len(x_test)):
+    #Getting the label for the current image
+    label = y_test[image]
 
-plt.imshow(x_test[19,:,:], cmap='gray', vmin=0, vmax=1)
+    if label not in imagesEachClass:
+        imagesEachClass[label] = x_test[image]
+    if len(imagesEachClass) == 10:
+        #Stopping at 10 because only 10 classes
+        break
+
+#Creating and formatting the images
+plt.figure(figsize=(10,10))
+for image in range(10):
+    plt.subplot(1, 10, image+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(imagesEachClass[image], cmap='gray', vmin=0, vmax=1)
+    plt.xlabel(classes[image])
 plt.show()
-### Need to finish this section, going to see if there are any tips during lab intro. ###
 
 #Setting the model
 model = models.Sequential()
 
-
 #Adding layers
 #Note we need to flatten the image to a vector, to serve as the input layer of the network
-model.add(layers.Flatten(28x28))
+model.add(layers.Flatten())
 
 #Creating the hidden layer with 512 nodes and ReLU activation function
 model.add(layers.Dense(512, activation='relu'))
@@ -37,20 +55,12 @@ model.add(layers.Dense(512, activation='relu'))
 #Creating the output layer with 10 nodes and softmax activation function
 model.add(layers.Dense(10, activation='softmax'))
 
-
 #Printing summary and compiling the model with adam optimizer and sparse... loss function  
 model.summary()
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-
 #Training the model for 5 epochs and a batch size of 32
 model.fit(x_train, y_train, epochs=5, batch_size=32)
-
-
-#Calculating the test's loss and accuracy
-loss, accuracy = model.evaluate(x_test, y_test)
-print(accuracy)
-
 
 #Calculating the model's predicted probalilty 
 predProbablility = model.predict(x_test)
@@ -58,9 +68,14 @@ predProbablility = model.predict(x_test)
 #y_test_hat = predicted value, using argmax() to find the class with the highest probability
 y_test_hat = np.argmax(predProbablility, axis=1)
 
-#2a: Give the recognition accuracy rate of the test set, and show the confusion matrix
-from sklearn.metrics import confusion_matrix
+#Calculating the test's loss and accuracy
+loss, accuracy = model.evaluate(x_test, y_test)
+print(accuracy)
 
-### Also need to finish this section. ###
-#Need to replace the ... with labels, like in mnist_cnn.py 
-#confuMatrix = confusion_matrix(y_test, y_test_hat, ...)
+#Creating and plotting the confusion matrix
+from sklearn.metrics import ConfusionMatrixDisplay
+ConfusionMatrixDisplay.from_predictions(y_test, y_test_hat)
+plt.show()
+
+#Printing the model summary
+model.summary()
